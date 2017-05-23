486786bfeed085243fc0f4c1470f6f8004fa7db5
bibaijin <bibaijin@gmail.com>
2017-05-23 12:38:55 +0800

    use redis-service-sm

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # hello-world
 
-一个 HTTP 服务，请求 `/` 时返回 `Hello, world.`。
+一个演示 [service](https://laincloud.gitbooks.io/white-paper/usermanual/service.html)
+用法的 LAIN 应用，提供 HTTP 服务，请求 `/` 时返回 `Hello, LAIN. You are the ${n}th visitor.`。
+
+> ${n} 指调用次数。
--- main.go
+++ main.go
@@ -3,11 +3,36 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strconv"
+
+	"github.com/go-redis/redis"
 )
 
+const visitCountKey = "visitCountKey"
+
 func main() {
+	redisClient := redis.NewClient(&redis.Options{
+		Addr: "redis:6379",
+	})
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Hello, world.")
+		if err := redisClient.Incr(visitCountKey).Err(); err != nil {
+			fmt.Fprintf(w, "redisClient.Incr() failed, error: %s.", err)
+			return
+		}
+
+		visitCountStr, err := redisClient.Get(visitCountKey).Result()
+		if err != nil {
+			fmt.Fprintf(w, "redisClient.Get() failed, error: %s.", err)
+			return
+		}
+
+		visitCount, err := strconv.Atoi(visitCountStr)
+		if err != nil {
+			fmt.Fprintf(w, "strconv.Atoi() failed, error: %s.", err)
+			return
+		}
+		fmt.Fprintf(w, "Hello, LAIN. You are the %dth visitor.", visitCount)
 	})
 
 	http.ListenAndServe(":8080", nil)
